c568cefec37b37d544641014c3dd543cf5a00647
FaroukAjimi <benajimi@gmail.com>
2020-06-25 14:16:45 +0100

    CONSOLE.SHOW

--- console.py
+++ console.py
@@ -4,6 +4,15 @@ import cmd
 from models.entities.player import Player
 from datetime import datetime
 from shlex import split
+import json
+
+def check():
+    with open("data.json", "r") as f:
+        s= f.read()
+        if (s == ""):
+            return True
+        else:
+            return False
 
 class Coderz(cmd.Cmd):
     prompt = "(Coderz) "
@@ -18,20 +27,46 @@ class Coderz(cmd.Cmd):
         return True
 
     def do_create(self, line):
+        exist = True
         my_list = line.split(" ")
         y = 0
         dic = {}
         player = eval("{}()".format(my_list[0]))
-        player.__dict__["name"] = my_list[1]
-        player.__dict__["email"] = my_list[2]
-        player.__dict__["password"] = my_list[3]
-        #(player).__dict__.update(dic)
-        #(player).save()
-        print(player.__dict__)
-        print("{}".format(player.id))
+        
+        try:
+            player.__dict__["name"] = my_list[1]
+            player.__dict__["email"] = my_list[2]
+            player.__dict__["password"] = my_list[3]
+        except:
+            print("Usage: create Player <GamerTag> <email> <Password>")
+
+        with open("data.json", 'r') as f:
+            jsondata = (f.read())
+            jsn = '{{"data": [{}]}}'.format(jsondata)
+            jsn = json.loads(jsn)
+            for i in jsn["data"]:
+                for k, v in i.items():
+                    if i["name"] == my_list[1]:
+                        print("already existing name")
+                        exist = False
+                        break
+                    else:
+                        exist = True
+                
+        
+        checks = check();
+        if exist:
+            with open("data.json", "a") as f:
+                if checks:
+                    f.write("{}".format(json.dumps(player.__dict__)))
+                else:
+                    f.write(" ,{}".format(json.dumps(player.__dict__)))
+            
     def do_show(self, line):
         my_list = line.split(" ")
-        objects = storage.all()
-        
+        if len(my_list) == 1:
+             with open("data.json", 'r') as f:
+                 print(json.loads('{{"data": [{}]}}'.format(f.read())))
+                 
 if __name__ == "__main__":
     Coderz().cmdloop()
