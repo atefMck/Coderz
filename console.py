cd76ef8e568d15eb0ce4fdc15bf15f28e60bcac9
atefsama666 <mechken.atef@gmail.com>
2020-06-26 10:16:14 +0100

    Stashing stuff

--- console.py
+++ console.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
 """This is the console for COderz"""
 import cmd
-from models.entities.player import Player
+from models.player import Player
 from datetime import datetime
 from shlex import split
 import json
 
+
 def check():
     with open("data.json", "r") as f:
-        s= f.read()
+        s = f.read()
         if (s == ""):
             return True
         else:
             return False
 
+
 class Coderz(cmd.Cmd):
     prompt = "(Coderz) "
     all_classes = {"Player"}
+
     def emptyline(self):
         pass
 
@@ -32,7 +35,7 @@ class Coderz(cmd.Cmd):
         y = 0
         dic = {}
         player = eval("{}()".format(my_list[0]))
-        
+
         try:
             player.__dict__["name"] = my_list[1]
             player.__dict__["email"] = my_list[2]
@@ -52,21 +55,21 @@ class Coderz(cmd.Cmd):
                         break
                     else:
                         exist = True
-                
-        
-        checks = check();
+
+        checks = check()
         if exist:
             with open("data.json", "a") as f:
                 if checks:
                     f.write("{}".format(json.dumps(player.__dict__)))
                 else:
                     f.write(" ,{}".format(json.dumps(player.__dict__)))
-            
+
     def do_show(self, line):
         my_list = line.split(" ")
         if len(my_list) == 1:
-             with open("data.json", 'r') as f:
-                 print(json.loads('{{"data": [{}]}}'.format(f.read())))
-                 
+            with open("data.json", 'r') as f:
+                print(json.loads('{{"data": [{}]}}'.format(f.read())))
+
+
 if __name__ == "__main__":
     Coderz().cmdloop()
