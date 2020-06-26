16f51f6079ae604d4fd05e1599886e143be1aed7
FaroukAjimi <benajimi@gmail.com>
2020-06-26 02:09:39 +0100

    S

---
+++ web-app/app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+from flask import Flask, render_template, request
+import json
+app = Flask(__name__)
+def signup(data):
+    if not data:
+        return False
+    data.pop('signup')
+    email = data['email']
+    jsonDB = open('data.json', 'r')
+    DBcheck = jsonDB.read()
+    if email in DBcheck:
+        jsonDB.close()
+        return (False, 'This user is already registred')
+    else:
+        jsonDB = open('data.json', 'a+')
+        jsonDB.write(" ,{}".format(json.dumps(data)))
+        jsonDB.close()
+        return ('success')
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.form:
+        data = request.form.to_dict()
+        if "signup" in data:
+            response = signup(data)
+            return render_template(
+                'index.html',
+                response=response
+            ) 
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
